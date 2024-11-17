249d167b7f0d7d50694bc2e753ca65a07c5ad5ed
kabinh07 <kanchon@polygontech.xyz>
2024-11-17 16:03:30 +0600

    yolo model train

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from modules.model import YOLOModel
 def main():
     with open('configs/config.yaml') as f:
         config = Dict(yaml.safe_load(f))
-    print(config)
     model = YOLOModel(config)
     parser = argparse.ArgumentParser()
     parser.add_argument("method", type=str, help="type if want to train, test, predict etc.")
--- modules/model.py
+++ modules/model.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 import os
+import subprocess
 
 class YOLOModel:
     def __init__(self, config):
         self.config = config
-        self.model = YOLO(self.config.model_dir)
-        self.data_dir = f'{self.config.data_dir}'
-        self.project_name = os.path.join('runs', self.config.project_name)
+        self.model = YOLO(self.config.model.model_dir)
+        self.data_dir = f'{self.config.model.data_dir}'
+        self.project_name = os.path.join('runs', self.config.model.project_name)
     
     def train(self):
         config = {}
@@ -15,7 +16,8 @@ class YOLOModel:
                 config[key] = value
         if not 'data' in config.keys():
             config['data'] = os.path.join(self.data_dir, 'data.yaml')
-        config['project	'] = self.project_name
+        config['project'] = self.project_name
+        print(config)
         self.model.train(**config)
         return
 
@@ -24,7 +26,7 @@ class YOLOModel:
         for key, value in self.config.predict.items():
             if not value is None:
                 config[key] = value
-        config['project	'] = self.project_name
+        config['project'] = self.project_name
         self.model.predict(**config)
         return
         
